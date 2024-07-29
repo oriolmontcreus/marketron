b211b4e5c3f53886548815c055c340b2a6986fc2
oriolmontcreus <licensedgamer641319@gmail.com>
2024-07-29 21:12:02 +0200

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+		  keyframes: {
+			slide: {
+			  from: { transform: 'translateX(0%)' },
+			  to: { transform: 'translateX(-100%)' },
+			},
+			'slide-reverse': {
+			  from: { transform: 'translateX(-100%)' },
+			  to: { transform: 'translateX(0%)' },
+			},
+		  },
+		  animation: {
+			slide: 'slide 50s linear infinite',
+			'slide-reverse': 'slide-reverse 50s linear infinite',
+		  },
+		},
+	},
+	plugins: [],
+}
