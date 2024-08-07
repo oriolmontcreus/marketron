5139428259e95e45c06d8e349667e83aa38a7c7f
oriolmontcreus <licensedgamer641319@gmail.com>
2024-08-07 19:55:51 +0200

    fix: fixed responsive feature notifications

--- src/sections/FeatureNotifications.tsx
+++ src/sections/FeatureNotifications.tsx
@@ -11,27 +11,26 @@ const Notification = ({ name, description, icon, color, time }: Notification) =>
   return (
     <figure
       className={cn(
-        "relative mx-auto min-h-fit w-full max-w-[400px] cursor-pointer overflow-hidden rounded-lg border-2 border-black p-4",
+        "relative mx-auto min-h-fit w-full max-w-[400px] cursor-pointer overflow-hidden rounded-lg border-2 border-black p-3 sm:p-4",
         "transition-all duration-200 ease-in-out hover:scale-[103%] bg-white",
-
       )}
     >
-      <div className="flex flex-row items-center gap-3">
+      <div className="flex flex-row items-center gap-2 sm:gap-3">
         <div
-          className="flex size-10 items-center justify-center rounded-lg"
+          className="flex size-8 sm:size-10 items-center justify-center rounded-lg"
           style={{
             backgroundColor: color,
           }}
         >
-          <span className="text-lg">{icon}</span>
+          <span className="text-base sm:text-lg">{icon}</span>
         </div>
         <div className="flex flex-col overflow-hidden">
-          <figcaption className="flex flex-row items-center whitespace-pre text-lg font-medium text-gray-950">
+          <figcaption className="flex flex-row items-center whitespace-pre text-sm sm:text-lg font-medium text-gray-950">
             <span className="text-sm sm:text-lg">{name}</span>
             <span className="mx-1">·</span>
             <span className="text-xs text-gray-500">{time}</span>
           </figcaption>
-          <p className="text-sm font-normal text-gray-950">
+          <p className="text-xs sm:text-sm font-normal text-gray-950">
             {description}
           </p>
         </div>
@@ -48,7 +47,7 @@ export function FeatureNotifications({
   return (
     <div
       className={cn(
-        "relative flex h-[500px] w-full max-w-[400px] flex-col p-6 overflow-hidden rounded-lg border-2 border-black bg-background md:shadow-xl",
+        "relative flex h-[400px] sm:h-[500px] w-full max-w-[350px] sm:max-w-[400px] flex-col p-3 sm:p-6 overflow-hidden rounded-lg border-2 border-black bg-background md:shadow-xl",
         className,
       )}
     >
