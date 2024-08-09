8887ed463845b5939520afdd3af0a9859ad0e01e
oriolmontcreus <licensedgamer641319@gmail.com>
2024-08-09 21:31:01 +0200

    feat: added oops page

--- src/components/RibbonLogos.tsx
+++ src/components/RibbonLogos.tsx
@@ -68,7 +68,7 @@ const TranslateWrapper = ({
 const LogoItem = ({ Icon, name }: { Icon: IconType; name: string }) => {
   return (
     <a
-      href="/"
+      href="/oops"
       rel="nofollow"
       target="_blank"
       className="flex items-center justify-center gap-4 px-4 py-4 text-black transition-colors hover:bg-neutral-200 md:py-6"
